95dbcdd2c3f8e5947b0920627742da3a8b3c2ab0
Mathicraft <abbetmathieu.am@gmail.com>
2026-05-04 15:39:19 +0200

    Correction async

--- backend/main.py
+++ backend/main.py
@@ -24,11 +24,11 @@ def get_db():
         db.close()
         
 @app.get("/personnages") 
-async def liste_personnages(db: Session = Depends(get_db)):
+def liste_personnages(db: Session = Depends(get_db)):
     return db.query(Personnage).all()
 
 @app.post("/personnages")
-async def creer_peronnage(perso_data: model.Personnage, db: Session = Depends(get_db)):
+def creer_peronnage(perso_data: model.Personnage, db: Session = Depends(get_db)):
     new_personnage = Personnage(
         nom = perso_data.nom, classe = perso_data.classe, niveau = perso_data.niveau, pv = perso_data.pv, attaque = perso_data.attaque, defense = perso_data.defense
     )
@@ -43,19 +43,22 @@ async def creer_peronnage(perso_data: model.Personnage, db: Session = Depends(ge
         return f"Erreur: {e}"
     
 @app.delete("/personnages/{id}")
-async def supprimer_personnage(id: int,db: Session = Depends(get_db)):
-    if not id:
-        raise HTTPException(status_code=404, detail="Item non trouvé")
+def supprimer_personnage(id: int, db: Session = Depends(get_db)):
     personnage = db.query(Personnage).filter(Personnage.id == id).first()
+    if not personnage:
+        raise HTTPException(status_code=404, detail="Personnage non trouvé")
+    
     try:
         db.delete(personnage)
         db.commit()
+        return {"message": f"Personnage {id} supprimé avec succès"}
     except Exception as e:
         db.rollback()
-        return f"Erreur: {e}"
+        raise HTTPException(status_code=500, detail=str(e))
+
 
 @app.patch("/personnages/{id}/levelup")
-async def level_up(id: int, db: Session = Depends(get_db)):
+def level_up(id: int, db: Session = Depends(get_db)):
 #   niveau   += 1
 #   pv       += 20
 #   attaque  += 5
@@ -75,5 +78,5 @@ async def level_up(id: int, db: Session = Depends(get_db)):
         return personnage
     except Exception as e:
         db.rollback()
-        return f"Erreur: {e}"
+        raise HTTPException(status_code=500, detail=str(e))
     
